58d58d4cd92200797cafa0249c1041b8558ab549
Jongin Yang <yji8835@naver.com>
2023-12-01 17:02:19 +0900

    변경

--- BankServer/src/bank/SavingAccount.java
+++ BankServer/src/bank/SavingAccount.java
@@ -1,5 +1,7 @@
 package bank;
 
+import common.AccountType;
+
 abstract class Account{
     abstract String display();
 
@@ -9,8 +11,16 @@ public class SavingAccount extends Account{ //저축예금계좌
     private double InterestRate;
     private long MaxTransferAmountToChecking;
     private AccountVO account;
+    public SavingAccount(double inter, long maxtransfer){
+        this.InterestRate = inter;
+        this.MaxTransferAmountToChecking = maxtransfer;
+    }
+
     @Override
     String display() {
+        if(InterestRate != 0) {
+            return null;
+        }
         return null;
     }
 }
